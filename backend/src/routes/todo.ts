c89bd192be1b63e707e6a7cf0884689b55d35a96
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-04-23 18:29:52 +0100

    Route: added create new todo route

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -3,4 +3,14 @@ import { Todo } from "../db/models/Todo";
 
 const router = express.Router();
 
+// create a new todo router
+router.post("/", async (req, res) => {
+  const todo = req.body.todo;
+  const newTodo = await Todo.query()
+    .insert({ description: todo.description })
+    .returning("*");
+
+  res.send({ newTodo });
+});
+
 export default router;
